51acc8321e9b0cd0e1eb7ee89f2c99303eb9ec33
Leonardo Luz <leonardoluz10@hotmail.com>
2023-11-21 19:10:26 -0300

    feat: :white_check_mark: Criando manipulador de erros no modelo autor e livro.

--- src/middlewares/manipuladorDeErros.js
+++ src/middlewares/manipuladorDeErros.js
@@ -3,10 +3,15 @@ import mongoose from "mongoose";
 // eslint-disable-next-line no-unused-vars
 function manipuladorDeErros(error, req, res, next) {
   if (error instanceof mongoose.Error.CastError) {
-    res.status(400).json({message: "Um ou mais dados fornecido estão incorretos."});
+    res.status(400).json({ message: "Um ou mais dados fornecido estão incorretos." });
+  } else if (error instanceof mongoose.Error.ValidationError) {
+    const mensagensErro = Object.values(error.errors)
+      .map(erro => erro.message)
+      .join("; "); 
+    res.status(400).json({message: `O seguintes erros foram encontrados: ${mensagensErro}`});
   } else {
     res.status(500).json({ message: `${error.message} - Falha ao solicitar dados por id` });
   }
 }
 
-export default manipuladorDeErros;
+export default manipuladorDeErros; 
--- src/models/Livro.js
+++ src/models/Livro.js
@@ -2,11 +2,21 @@ import mongoose from "mongoose";
 
 const livroSchema = new mongoose.Schema({
   id: { type: mongoose.Schema.Types.ObjectId },
-  titulo: { type: mongoose.Schema.Types.String, required: true },
-  editora: { type: mongoose.Schema.Types.String },
+  titulo: { 
+    type: mongoose.Schema.Types.String, 
+    required: [true, "O titulo do Livro é obrigatório"] 
+  },
+  editora: { 
+    type: mongoose.Schema.Types.String ,
+    required:[true, "Editora é obrigatório"] 
+  },
   preco: { type: Number },
   paginas: { type: Number },
-  autor: { type: mongoose.Schema.Types.ObjectId, ref: "autores", required: true }
+  autor: { 
+    type: mongoose.Schema.Types.ObjectId, 
+    ref: "autores", 
+    required: [true, "O autor é obrigatório"] 
+  }
 }, { versionKey: false });
 
 const livro = mongoose.model("livros", livroSchema);
